441d0cef1ad275dc3044eb78cb2662756ae07c3f
swaraj behera <swarajbehera99@gmail.com>
2021-05-10 16:27:18 +0530

    secondcommit

--- src/main/java/com/cts/Main.java
+++ src/main/java/com/cts/Main.java
@@ -1,9 +1,14 @@
 package com.cts;
 
+import com.cts.model.Person;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		
+		Person person = new Person();
+		person.setName("New updated line !!!");
+		System.out.println(person.getName());
 
 	}
 
